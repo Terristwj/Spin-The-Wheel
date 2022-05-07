f82e448112f6a519671e8e9a3d9eef5c496e7f04
Terris <27739101+Terristwj@users.noreply.github.com>
2022-05-08 07:53:42 +0800

    Fixed bug: AdminConrols to change width.

--- scripts/script.js
+++ scripts/script.js
@@ -12,41 +12,46 @@ function changeView(){
 function changeTheme(){
     var isAdmin = document.getElementById("Toggle").checked;
     // Change to Admin
-    if (isAdmin){
+    if (isAdmin)
         document.getElementById("theme").href = theme_href = "styles/adminTheme.css";
-        document.getElementById("AdminControls").classList.remove("invisible");
-    }
     // Change to User
-    else{
+    else
         // Checks to see if settings is opened
        isSettingsOpen();
-       document.getElementById("AdminControls").classList.add("invisible");
-    }
 }
 
 // Open admin settings
 function openSettings(){
     var isAdmin = document.getElementById("Toggle").checked;
-    var closed = document.getElementById("AdminSettings").classList.contains('close-AdminSettings');
-    // Open settings
-    if (closed && isAdmin)
+    var toOpen = document.getElementById("AdminSettings").classList.contains('close-AdminSettings');
+    // Open settings & increase width of invisible panel
+    if (toOpen && isAdmin){
         document.getElementById("AdminSettings").classList.remove('close-AdminSettings');
-    // Close settings
-    else
+        document.getElementById("AdminControls").classList.add("width330px");
+        document.getElementById("AdminControls").classList.remove("width90px");
+    }
+    // Close settings & decrease width of invisible panel
+    else{
         document.getElementById("AdminSettings").classList.add('close-AdminSettings');
+        document.getElementById("AdminControls").classList.add("width90px");
+        document.getElementById("AdminControls").classList.remove("width330px");
+    }
 }
 
 // Force close admin settings(if opened)
 function isSettingsOpen(){
-    var opened = !document.getElementById("AdminSettings").classList.contains('close-AdminSettings');
-    if (opened){
+    var toClose = !document.getElementById("AdminSettings").classList.contains('close-AdminSettings');
+    if (toClose){
         document.getElementById("AdminSettings").classList.add('close-AdminSettings');
         // Changes theme after closing admin settings
         setTimeout(function() {
             document.getElementById("theme").href = "styles/userTheme.css";
         }, 750);
+        // Decrease width of invisible panel
+        document.getElementById("AdminControls").classList.add("width90px");
+        document.getElementById("AdminControls").classList.remove("width330px");
     } 
-    // Changes
+    // Changes theme only
     else
         document.getElementById("theme").href = "styles/userTheme.css";
 }
